d8476e8aac0f34fcb57daa688de9236d2a5459dc
sarah390 <146526528+sarah390@users.noreply.github.com>
2023-10-03 12:50:38 +0000

    save

--- script.js
+++ script.js
@@ -1,55 +1,57 @@
 const pictures = ["pic1.png", "pic2.png", "pic3.png", "pic4.png", "pic5.png", "pic6.png"];
 const field = [4, 1, 2, 0, 3, 5, 1, 0, 3, 4, 2, 5];
 
-let index = 0;
 let flippedCards = [];
 let canFlip = true;
-let matchedPairs = [];
 
-for (let td of document.querySelectorAll("tbody td")) {
-    const i = index;
+document.querySelectorAll("tbody td").forEach((td, index) => {
     td.addEventListener("click", function () {
-        if (!canFlip || flippedCards.length >= 2 || td === flippedCards[0]) return;
+        if (!canFlip || flippedCards.length >= 2) return;
 
-        td.style.backgroundImage = "url(" + pictures[field[i]] + ")";
-        td.style.color = "transparent";
-        flippedCards.push(td);
+        const i = index;
 
-        if (flippedCards.length === 2) {
-            canFlip = false;
-            setTimeout(() => {
-                const [firstCard, secondCard] = flippedCards;
-                const index1 = i;
-                const index2 = Number(secondCard.dataset.index);
+        if (!td.classList.contains('flipped')) {
+            td.classList.add('flipped');
+            td.style.backgroundImage = "url(" + pictures[field[i]] + ")";
+            td.style.color = "transparent";
 
-                if (field[index1] === field[index2]) {
-                    // Cards match
-                    matchedPairs.push(field[index1]);
-                } else {
-                    // Cards do not match, flip them back
-                    firstCard.style.backgroundImage = "";
-                    secondCard.style.backgroundImage = "";
-                    firstCard.style.color = "";
-                    secondCard.style.color = "";
-                }
+            flippedCards.push({ td, index });
 
-                flippedCards = [];
-                canFlip = true;
+            if (flippedCards.length === 2) {
+                canFlip = false;
 
-                // Check if all pairs are matched
-                if (matchedPairs.length === field.length / 2) {
-                    alert("Congratulations! You've matched all pairs.");
-                }
-            }, 1500); // Adjust the delay time (in milliseconds) as needed
+                setTimeout(() => {
+                    const [firstCard, secondCard] = flippedCards;
+                    const index1 = firstCard.index;
+                    const index2 = secondCard.index;
+
+                    if (field[index1] !== field[index2]) {
+                        // Cards do not match, flip them back
+                        firstCard.td.classList.remove('flipped');
+                        secondCard.td.classList.remove('flipped');
+                        firstCard.td.style.backgroundImage = "";
+                        secondCard.td.style.backgroundImage = "";
+                        firstCard.td.style.color = "";
+                        secondCard.td.style.color = "";
+                    }
+
+                    flippedCards = [];
+                    canFlip = true;
+                     // Überprüfen, ob alle Paare übereinstimmen
+                     if (document.querySelectorAll(".flipped").length === field.length) {
+                        // Alle Karten sind umgedreht, Spiel beendet
+                        showCongratulationsMessage();
+                    }
+                }, 1500); // Adjust the delay time (in milliseconds) as needed
+            }
         }
     });
-
-    // Set a custom data attribute to store the index
-    td.dataset.index = index;
+});
 
     index++;
-}
 
-// Hide the "Congratulations!" message initially
-const overlay = document.querySelector('.overlay');
-overlay.classList.add('hidden');
+    function showCongratulationsMessage() {
+        alert("Congratulations! You've matched all pairs.");
+        const overlay = document.querySelector('.overlay');
+    overlay.classList.remove('hidden');
+        }
